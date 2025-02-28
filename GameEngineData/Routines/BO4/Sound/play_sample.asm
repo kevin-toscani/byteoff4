01c14d0cb8c5766699d50d3cc6a8d4e1ca2312fd
Kevin81 <kevin.van.der.burg@gmail.com>
2025-02-28 10:58:52 +0100

    Possible hiccup fix in sample playback

--- GameEngineData/Routines/BO4/Sound/play_sample.asm
+++ GameEngineData/Routines/BO4/Sound/play_sample.asm
@@ -1,6 +1,11 @@
 
-SwitchBank #$1D
 
+;; Disable NMI
+LDA #$00
+STA $2000
+
+;; Play sample
+SwitchBank #$1D
     LDA $BFFE
     STA temp16
     LDA $BFFF
@@ -36,5 +41,11 @@ SwitchBank #$1D
         STA temp16+1        ; 218
         ORA temp16          ; 221
     BNE -loop               ; 224
-
 ReturnBank
+
+;; Enable NMI
+LDA camScreen
+AND #$01
+ORA #$90
+STA $2000
+
